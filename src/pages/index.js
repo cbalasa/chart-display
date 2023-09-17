2dbf9949e6de69cc2b3f4ab9e0de33beb241be48
cbalasa <c.balasa@gmail.com>
2023-09-17 19:11:08 +0300

    Gh Pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,22 @@
 import { Inter } from "next/font/google";
 import DisplayChart from "@/modules/chart/Chart";
+import Head from "next/head";
 
 export default function Home() {
-	return <DisplayChart />;
+	return (
+		<>
+			<Head>
+				<title>Chart Display</title>
+				<meta
+					name="description"
+					content="Simply display of a chart using Apex Charts and NextJs"
+				/>
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"
+				/>
+			</Head>{" "}
+			<DisplayChart />
+		</>
+	);
 }
